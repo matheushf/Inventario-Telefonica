9039638043f2d9c23214ee7c886d9aa2aadaa4db
Matheus Victor <hffmatheus@gmail.com>
2016-03-23 17:57:01 -0300

    adjustments

--- lib/MetodosUtil.php
+++ lib/MetodosUtil.php
@@ -75,10 +75,11 @@ function ImportarCSV($Posicoes, $ArquivoNome) {
 //    _debug($arquivo);
 
     $row = 1;
-    if (($handle = fopen(DOCUMENT_ROOT . '/csv/etiquetas.csv', "r")) !== FALSE) {
+    $handle = fopen(DOCUMENT_ROOT . '/csv/etiquetas.csv', "r");
+    if ($handle !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $num = count($data);
-//            echo "<p> $num fields in line $row: <br /></p>\n";
+
             $row++;
             for ($c = 0; $c < $num; $c++) {
                 echo $data[$c] . "<br />\n";
